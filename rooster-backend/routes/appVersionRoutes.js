b890ea1c339ec5fa99efe8b3bbd2f83430ae1193
Ajaya <ajaya.shrestha.521@gmail.com>
2025-08-06 10:56:14 +0545

    Update backend to expect v1.0.9+9 for next update cycle

--- rooster-backend/routes/appVersionRoutes.js
+++ rooster-backend/routes/appVersionRoutes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 // App version configuration
 const APP_VERSIONS = {
   android: {
-    latest_version: '1.0.7',
-    latest_build_number: '7',
+    latest_version: '1.0.9',
+    latest_build_number: '9',
     update_required: false,
     update_message: 'A new version of SNS Rooster is available with training management features and bug fixes!',
     download_url: 'https://sns-rooster.onrender.com/api/app/download/android/file',
@@ -13,8 +13,8 @@ const APP_VERSIONS = {
     min_required_build: '1',
   },
   web: {
-    latest_version: '1.0.7',
-    latest_build_number: '7',
+    latest_version: '1.0.9',
+    latest_build_number: '9',
     update_required: false,
     update_message: 'A new version of SNS Rooster is available with training management features. Please refresh your browser.',
     download_url: 'https://sns-rooster.onrender.com/api/app/download/android/file',
@@ -236,6 +236,7 @@ function _isUpdateRequired(currentVersion, currentBuild, minRequiredVersion, min
 }
 
 module.exports = router; 
+
 
 
 
